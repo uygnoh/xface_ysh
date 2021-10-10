907060f7b1327e6f6e0a47ec7aef8b8b4cfc360a
hongyu <961832097@qq.com>
2021-10-10 09:56:28 +0800

    Sun Oct 10 09:56:27 HKT 2021

--- embedded/__LIBRARY__/libraries/ds18b20/ds18b20.c
+++ embedded/__LIBRARY__/libraries/ds18b20/ds18b20.c
@@ -1,8 +1,5 @@
 #include "ds18b20.h"
 
-//定义(low_byte, high_byte)为ds18b20温度变量
-uint8_t low_byte;
-uint8_t high_byte;
 
 /*******************************************************************************
   函数名称: ds18b20_init()
@@ -13,12 +10,12 @@ uint8_t high_byte;
 uint8_t ds18b20_init(void)
 {
         uint8_t flag = 0;
-        DQ = 1;         //释放总线
-        DQ = 0;         //A点：拉低总线
+        DQ = 1;         //单片机释放总线
+        DQ = 0;         //A点 ：拉低总线
         delay_us(500);  //延时500us，(480us~960us)
-        DQ = 1;         //B点：释放总线
+        DQ = 1;         //B点 ：释放总线
         delay_us(68);   //BM点：延时68us，等待DQ回应
-        if (DQ == 0) {  //判断“DQ线上的数据”
+        if (!DQ) {      //判断“DQ线上的数据”
                 flag = 0x00;
         } else {
                 flag = 0x01;
@@ -29,57 +26,57 @@ uint8_t ds18b20_init(void)
 }
 
 /*******************************************************************************
-  函数名称: ds18b20_write_dat()
-  输入参数: cmd，要写入的命令字节
+  函数名称: ds18b20_write()
+  输入参数: dat（要写入的命令字节）
   输出参数: 无
-  函数功能: 向ds18b20写入一个字节的 COMMANDS，读写都是从最低位开始
+  函数功能: 向ds18b20写入一个字节的数据（读写都是从最低位开始）
 *******************************************************************************/
-void ds18b20_write_cmd(uint8_t cmd)
+void ds18b20_write(uint8_t dat)
 {
         uint8_t tmp;
         for (tmp = 0x01; tmp != 0; tmp <<= 1) {
-                DQ = 0;         //单片机拉低总线
-                delay_us(2);    //延时2us
-                if ((tmp & cmd) == 0) {
-                        DQ = 0;
+                DQ = 0;                 //单片机拉低总线
+                delay_us(2);            //延时2us
+                if ((tmp & dat) == 0) { //判断写入的数据是“0”还是“1”
+                        DQ = 0;         //如果是“0”，向“DQ总线上”写“0”
                 } else {
                         DQ = 1;
                 }
-                delay_us(60);   //延时60us，等待DS18B20来采集数据
-                DQ = 1;         //释放总线
+                delay_us(60);           //延时60us，等待DS18B20来采集数据
+                DQ = 1;                 //释放总线
         }
 }
 
 /*******************************************************************************
-  函数名称: ds18b20_read_dat()
+  函数名称: ds18b20_read()
   输入参数: 无
-  输出参数: 返回读出的一个字节的数据
-  函数功能: 从ds18b20中读取一个字节的数据，读写都是从最低位开始
+  输出参数: 返回从ds18b20中读出的一个字节的数据
+  函数功能: 从ds18b20中读取一个字节的数据（读写都是从最低位开始）
 *******************************************************************************/
-int8_t ds18b20_read_dat(void)
+int8_t ds18b20_read(void)
 {
         uint8_t dat = 0;
         uint8_t tmp;
         for (tmp = 0x01; tmp != 0; tmp <<= 1) {
-                DQ = 0;         //单片机拉低总线
-                delay_us(2);    //延时2us
-                DQ = 1;         //释放总线
-                delay_us(2);    //延时2us, 等待DS18B20拉低或拉高
-                if (DQ) {       //读取DQ总线上的位数据 
+                DQ = 0;                 //单片机拉低总线
+                delay_us(2);            //延时2us
+                DQ = 1;                 //单片机释放总线
+                delay_us(2);            //延时2us, 等待DS18B20拉低或拉高
+                if (DQ) {               //读取DQ总线上的位数据 
                         dat |= tmp;
                 } else {
                         dat &= ~tmp;
                 }
-                delay_us(60);   //延时60us
+                delay_us(60);           //延时60us，等待总线释放
         }
         return dat;
 }
 
 /*******************************************************************************
-  函数名称: ds18b20_read_temperature()
+  函数名称: ds18b20_temperature_data()
   输入参数: 无
-  输出参数: 返回“16位的温度数据”
-  函数功能: 从scratchpad中读取温度数据并保存到全局变量中
+  输出参数: 返回“16位的有符号温度数据”
+  函数功能: 从scratchpad中读取温度数据
   _________________________________________________________
   ds18b20单总线器件驱动程序编写流程(总线中只有一只ds18b20的情况下)
   _________________________________________________________
@@ -87,39 +84,45 @@ int8_t ds18b20_read_dat(void)
   第二步: ROM命令(指令寻找和匹配，指定待操作器件)
   第三步: 功能命令(进行具体操作或传输数据)
 *******************************************************************************/
-uint16_t ds18b20_read_temperature(void)
+int16_t ds18b20_temperature_data(void)
 {
         uint8_t low_byte, high_byte;
-        uint16_t temperature;
-        if (ds18b20_init()) {                   //判断ds18b20是否初始化成功
-                ds18b20_write_cmd(0xCC);        //skip rom
-                ds18b20_write_cmd(0x44);        //启动温度转换
+        int16_t temperature;                   //16位有符号数据
+        if (!ds18b20_init()) {                 //判断ds18b20是否初始化成功(0成功)
+                ds18b20_write(0xCC);           //skip rom
+                ds18b20_write(0x44);           //启动温度转换
+        } else {
+                printf("ds18b20_initialize_failed!\r\n");
         }
-        delay_ms(750);                          //延时750毫秒
-        if (ds18b20_init()) {                   //判断ds18b20是否初始化成功
-                ds18b20_write_cmd(0xCC);        //skip rom
-                ds18b20_write_cmd(0xBE);        //read scratchpad
-                low_byte   = ds18b20_read_dat();//read byte0
-                high_byte  = ds18b20_read_dat();//read byte1
-                temperature= ((uint16_t)high_byte << 8) + low_byet;
+        delay_ms(750);                         //延时750毫秒
+        if (!ds18b20_init()) {                 //判断ds18b20是否初始化成功(0成功)
+                ds18b20_write(0xCC);           //skip rom
+                ds18b20_write(0xBE);           //read scratchpad
+                low_byte  = ds18b20_read();    //read byte0
+                high_byte = ds18b20_read();    //read byte1
+                temperature = (int16_t(high_byte) << 8) + low_byte;
+        } else {
+                printf("ds18b20_initialize_failed!\r\n");
         }
         return(temperature);
 }
 
-
-
-//uint8_t ROM[8];
-//从ds18b2020中读出64位ROM码
-//*prom读出64位ROM码存放的地方
-//64位ROM码: 低位在前，高位在后读出
-void ds18b20_read_rom_code(uint8_t *prom)
+/*******************************************************************************
+  函数名称: ds18b20_rom_data()
+  输入参数: 无
+  输出参数: 无
+  函数功能: 从ds18b2020中读出64位ROM码（64位ROM码，低位在前，高位在后读出）
+*******************************************************************************/
+void ds18b20_rom_data(void)
 {
         uint8_t falg = 0;
         uint8_t i    = 0;
-        if (ds18b20_init()) {
-                ds18b20_write_cmd(READ_ROM);    //读取ROM指令
-                for (i = 0; i < 8; i++) {       //读取64位ROM码
-                        *prom++ = ds18b20_read_dat();
+        if (!ds18b20_init()) {
+                ds18b20_write(READ_ROM);        //读取ROM指令
+                for (i = 0; i < 8; i++) {       //读取64位ROM代码
+                        rom_code[i] = ds18b20_read();
                 }
+        } else {
+                printf("ds18b20_initialize_failed!\r\n");
         }
 }
