82d5d6f428434ce8af31abaf4e219efda8ee48d9
hongyu <961832097@qq.com>
2021-10-19 16:11:38 +0800

    Tue Oct 19 16:11:38 HKT 2021

--- embedded/__STM32__/STM32标准库/DMA/mem_to_mem.c
+++ embedded/__STM32__/STM32标准库/DMA/mem_to_mem.c
@@ -28,5 +28,56 @@ uint32_t destination_buf[BUFFER_SIZE];
 *******************************************************************************/
 void dma_setup(void)
 {
-        DMA_
+        DMA_InitTypeDef Y;
+        RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);      //打开DMA1时钟
+        Y.DMA_PeripheralBaseAddr = src_const_buf;               //配置外设为源
+        Y.DMA_MemoryBaseAddr     = destination_buf;             //SRAM为目的地
+        Y.DMA_DIR                = DMA_DIR_PeriphealSRC;        //FLASH => SRAM
+        Y.DMA_BufferSize         = BUFFER_SIZE;                 //传输数据大小
+        Y.DMA_PeriphealInc       = DMA_PeripheralInc_Enable;    //配置外设为地址增量模式
+        Y.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Word; //数据宽度为（32位)
+        Y.DMA_MemoryInc          = DMA_MemoryInc_Enable;        //配置SRAM为地址增量模式
+        Y.DMA_MemoryDataSize     = DMA_MemoryDataSize_Word;     //数据宽度为（32位)
+        Y.DMA_Mode               = DMA_Mode_Normal;             //配置DMA模式
+        Y.DMA_Priority           = DMA_Priority_High;           //配置DMA优先级
+        Y.DMA_M2M                = DMA_M2M_Enable;              //配置DMA为内存到内存传输
+        DMA_Init(DMA1_Channel6, &Y);                            //配置DMA1的通道6
+        DMA_ClearFlag(DMA1_FLAG_TC6);                           //清除DMA1的通道6
+        DMA_Cmd(DMA1_Channel6, ENABLE);                         //使能DMA1的通道6
+}
+
+/*******************************************************************************
+  函数名称: buf2_cmp()
+  输入参数: 无
+  输出参数: 无
+  函数功能: 判断指定的两个数据是否相等
+          如果完全相等返回1， 只要其中一个不相等返回0
+*******************************************************************************/
+uint8_t buf2_cmp(const uint32_t *src_buf,
+                  uint32_t       *dst_buf,
+                  uint16_t        buf_len)
+{
+        while (buf_len--) {
+                if (*src_buf != *dst_buf) {
+                        return 0;
+                }
+                src_buf++;
+                dst_buf++;
+        }
+        return 1;
+}
+
+
+int main(void)
+{
+        uint8_t status;
+        dma_setup();
+        //检测传输是否完成
+        while (DMA_GetFlagStatus(DMA1_FLAG_TC6) == RESET);
+        status = buf2_cmp(src_const_buf, destination_buf, BUFFER_SIZE);
+        if (status == 0) {
+                //关闭LED
+        } else {
+                //打开LED
+        }
 }
