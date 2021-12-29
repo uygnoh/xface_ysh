2c86dbc0e6314ae3c95ac7c0ca3de7ba057371fc
hongyu <961832097@qq.com>
2021-12-29 21:29:45 +0800

    Wed Dec 29 21:29:45 HKT 2021

---
+++ embedded/__AVR__/main.c
@@ -0,0 +1,14 @@
+#define F_CPU                   (8000000UL)     //8MHz
+#include <avr/io.h>
+#include <avr/iom328p.h>
+#include <avr/delay.h>
+int main(void)
+{
+        DDRD = 0b11111111;
+        while (1) {
+                PORTD = 0b00000001;
+                _delay_ms(500);
+                PORTD = 0b00000000;
+                _delay_ms(500);
+        }
+}
