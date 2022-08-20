786f0d4f7d6fa46a3507cb11023e15976b8a2b7a
hongyu <961832097@qq.com>
2022-08-20 13:57:36 +0800

    Sat Aug 20 13:57:36 HKT 2022

---
+++ yushihu/FILE/RP2040/PIO/blink_pio/main.c
@@ -0,0 +1,34 @@
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "blink.pio.h"
+
+int main(void) {
+        static const uint led_pin = 25;
+        static const float pio_freq = 2000;     // Hz
+        
+        // 选择PIO硬件（PIO0 或 PIO1）
+        PIO pio = pio0;
+        
+        // Get first free state machine in PIO 0
+        uint sm = pio_claim_unused_sm(pio, true);
+        
+        // Add PIO program to PIO instructin memory.
+        // return with the memory offset of the program
+        uint offset = pio_add_program(pio, &blink_program);
+        
+        // Calculate the PIO clock divider
+        float div = (float)clock_get_hz(clk_sys) / pio_freq;
+        
+        //
+        blink_program_init(pio, sm, offset, led_pin, div);
+        
+        // Start running our PIO program int the state machine
+        pio_sm_set_enabled(pio, sm, true);
+        
+        
+        // Do nothing
+        while (true) {
+                sleep_ms(1000);
+        }
+}
