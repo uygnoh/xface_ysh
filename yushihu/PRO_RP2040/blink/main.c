d70b397ac757fb4b23af05f9560bfd2fcb3b2e63
hongyu <961832097@qq.com>
2022-08-22 09:32:06 +0800

    Mon Aug 22 09:32:06 HKT 2022

--- yushihu/PRO_RP2040/blink/main.c
+++ yushihu/PRO_RP2040/blink/main.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-int main() {
+int main()
+{
+        const uint led_pin = 25;
 
-    const uint led_pin = 25;
+        // Initialize LED pin
+        gpio_init(led_pin);
+        gpio_set_dir(led_pin, GPIO_OUT);
 
-    // Initialize LED pin
-    gpio_init(led_pin);
-    gpio_set_dir(led_pin, GPIO_OUT);
+        stdio_init_all();
 
-    stdio_init_all();
-
-    // Loop forever
-    while (true) {
-
-        // Blink LED
-        printf("Blinking!\r\n");
-        gpio_put(led_pin, true);
-        sleep_ms(1000);
-        gpio_put(led_pin, false);
-        sleep_ms(1000);
-    }
+        // Loop forever
+        while (true) {
+                // Blink LED
+                printf("Blinking!\r\n");
+                gpio_put(led_pin, true);
+                sleep_ms(1000);
+                gpio_put(led_pin, false);
+                sleep_ms(1000);
+        }
 }
