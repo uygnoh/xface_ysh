3265e3a9103aa39f92023fc150db891fb977d22e
hongyu <961832097@qq.com>
2022-09-08 16:21:10 +0800

    Thu Sep  8 16:21:10 HKT 2022

---
+++ embedded/gcc_arm/__STM32__/STM32_LIBS/STM32标准库/SDIO/MY_SDIO/sdio.c
@@ -0,0 +1,8 @@
+//      PC8     --->    DATA0
+//      PC9     --->    DATA1
+//      PC10    --->    DATA2
+//      PC11    --->    DATA3
+//
+//      PC12    --->    CLK
+//      PD2     --->    CMD
+
