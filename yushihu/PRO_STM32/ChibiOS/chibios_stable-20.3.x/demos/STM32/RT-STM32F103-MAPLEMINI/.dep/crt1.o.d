34d9b8ead683341911dad562a8aeeafcb6c5cd49
hongyu <961832097@qq.com>
2022-09-28 22:31:20 +0800

    Wed Sep 28 22:31:20 HKT 2022

---
+++ yushihu/PRO_STM32/ChibiOS/chibios_stable-20.3.x/demos/STM32/RT-STM32F103-MAPLEMINI/.dep/crt1.o.d
@@ -0,0 +1,50 @@
+build/obj/crt1.o: ../../../os/common/startup/ARMCMx/compilers/GCC/crt1.c \
+ /usr/lib/gcc/arm-none-eabi/7.3.1/include/stdint.h \
+ /usr/include/newlib/stdint.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/_intsup.h /usr/include/newlib/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h \
+ ../../../os/common/startup/ARMCMx/devices/STM32F1xx/cmparams.h \
+ ../../../os/hal/boards/MAPLEMINI_STM32_F103/board.h \
+ ../../../os/common/ext/ST/STM32F1xx/stm32f1xx.h \
+ ../../../os/common/ext/ST/STM32F1xx/stm32f103xb.h \
+ ../../../os/common/ext/ARM/CMSIS/Core/Include/core_cm3.h \
+ ../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_version.h \
+ ../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_compiler.h \
+ ../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_gcc.h \
+ ../../../os/common/ext/ST/STM32F1xx/system_stm32f1xx.h
+
+/usr/lib/gcc/arm-none-eabi/7.3.1/include/stdint.h:
+
+/usr/include/newlib/stdint.h:
+
+/usr/include/newlib/machine/_default_types.h:
+
+/usr/include/newlib/sys/features.h:
+
+/usr/include/newlib/_newlib_version.h:
+
+/usr/include/newlib/sys/_intsup.h:
+
+/usr/include/newlib/sys/_stdint.h:
+
+/usr/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h:
+
+../../../os/common/startup/ARMCMx/devices/STM32F1xx/cmparams.h:
+
+../../../os/hal/boards/MAPLEMINI_STM32_F103/board.h:
+
+../../../os/common/ext/ST/STM32F1xx/stm32f1xx.h:
+
+../../../os/common/ext/ST/STM32F1xx/stm32f103xb.h:
+
+../../../os/common/ext/ARM/CMSIS/Core/Include/core_cm3.h:
+
+../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_version.h:
+
+../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_compiler.h:
+
+../../../os/common/ext/ARM/CMSIS/Core/Include/cmsis_gcc.h:
+
+../../../os/common/ext/ST/STM32F1xx/system_stm32f1xx.h:
